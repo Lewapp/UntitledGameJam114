609b2e00f4460c5d8c9499b64906ba1fdb464897
Lewis <JMitchellLewis@outlook.com>
2025-08-08 19:53:36 +0100

    Removed collision on picked up blocks
    Also added multicoloured blocks

--- PortalTest114/Assets/_Game/_Scripts/PickUp.cs
+++ PortalTest114/Assets/_Game/_Scripts/PickUp.cs
@@ -8,6 +8,7 @@ public class PickUp : MonoBehaviour, IInteractable
 {
     #region Private Variables
     private Rigidbody rb; // Reference to the Rigidbody component for physics interactions
+    private BoxCollider boxCollider; // Reference to the BoxCollider component for interaction detection
     #endregion
 
     #region Unity Methods
@@ -15,6 +16,8 @@ public class PickUp : MonoBehaviour, IInteractable
     {
         // Ensure the Rigidbody component is present and set to non-kinematic by default
         rb = GetComponent<Rigidbody>();
+        // Ensure the BoxCollider component is present
+        boxCollider = GetComponent<BoxCollider>(); 
     }
     #endregion
 
@@ -27,13 +30,17 @@ public class PickUp : MonoBehaviour, IInteractable
         // Check if the interactor is null or if the parent is not set
         if (data.parent == null)
         {
-            transform.SetParent(null);
             rb.isKinematic = false; // Enable physics when released
+            boxCollider.enabled = true; // Re-enable the collider for further interactions
+
+            transform.SetParent(null);
             SetTeleportableState(true);
             return;
         }
 
         rb.isKinematic = true; // Disable physics when picked up
+        boxCollider.enabled = false; // Disable the collider to prevent further interactions
+
         transform.SetParent(data.parent); // Set the parent to the specified hold point
         transform.localPosition = Vector3.zero; // Reset position relative to the parent
         transform.localEulerAngles = Vector3.zero; // Reset rotation relative to the parent
